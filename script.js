8888cbf1d7972f886b47c6e4df9f580f3a8f387b
EmmanuelMhlanga <145326894+EmmanuelMhlanga@users.noreply.github.com>
2026-02-04 19:58:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-window.addEventListener('load', () => {
-    const preloader = document.getElementById('preloader');
-    setTimeout(() => {
-        preloader.style.opacity = '0';
-        setTimeout(() => preloader.style.display = 'none', 500);
-    }, 1000);
-});
-
-// Smooth Scroll for links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+document.addEventListener('DOMContentLoaded', () => {
+    // PRELOADER
+    const loader = document.getElementById('preloader');
+    window.addEventListener('load', () => {
+        loader.style.opacity = '0';
+        setTimeout(() => { loader.style.display = 'none'; }, 800);
     });
 });
